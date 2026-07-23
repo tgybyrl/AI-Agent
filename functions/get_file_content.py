ba46c97921428de9c301a0e5f884d3ae809a02c7
tgybyrl <tgybyrl48@gmail.com>
2026-07-23 11:19:45 +0300

    Trying to fix abs paths.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,13 +1,25 @@
+import os
 from config import MAX_CHARS
 
 def get_file_content(working_directory: str, file_path: str) -> str:
-  if os.path.commonpath(file_path, working_directory):
-    raise Exception(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
-  if not os.path.isfile(file_path):
-    raise Exception(f'Error: File not found or is not a regular file: "{file_path}"')
+  try:
+    working_dir_abs = os.path.abspath(working_directory)
 
 
-  with open(file_path, 'r') as f:
-    file_content_string = f.read(MAX_CHARS)
-  if f.read(1):
-    pass
+    if os.path.commonpath([file_path, working_directory]) == file_path:
+      raise Exception(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
+      
+    if not os.path.isfile(os.path.join(working_directory, file_path)):
+      raise Exception(f'Error: File not found or is not a regular file: "{file_path}"')
+
+
+    with open(file_path, 'r') as f:
+      content = f.read(MAX_CHARS)
+    if f.read(1):
+      content += f'[...File "{file_path}" truncated at {MAX_CHARS}]'
+    return content
+
+  except Exception as e:
+    return f'Error: {e}'
+
+

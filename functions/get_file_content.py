9381437c05421040ee943c9224b6f20cf68c8a2f
tgybyrl <tgybyrl48@gmail.com>
2026-07-23 11:35:35 +0300

    Update to file path code.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -4,16 +4,16 @@ from config import MAX_CHARS
 def get_file_content(working_directory: str, file_path: str) -> str:
   try:
     working_dir_abs = os.path.abspath(working_directory)
-    file_path_abs = os.path.abspath(file_path)
+    file_path_abs = os.path.abspath(os.path.join(working_dir_abs, file_path))
 
-    if os.path.commonpath([file_path_abs, working_dir_abs]) == working_dir_abs:
+    if os.path.commonpath([file_path_abs, working_dir_abs]) != working_dir_abs:
       raise Exception(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
 
-    if not os.path.isfile(os.path.normpath(os.path.join(working_dir_abs, file_path))):
+    if not os.path.isfile(os.path.normpath(file_path_abs)):
       raise Exception(f'Error: File not found or is not a regular file: "{file_path}"')
 
 
-    with open(file_path, 'r') as f:
+    with open(file_path_abs, 'r') as f:
       content = f.read(MAX_CHARS)
       if f.read(1):
         content += f'[...File "{file_path}" truncated at {MAX_CHARS}]'

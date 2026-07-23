c3e80b9c2bbe1f3e8c5e017afa6d1af2430480fb
tgybyrl <tgybyrl48@gmail.com>
2026-07-23 11:04:13 +0300

    Reading content and setting maximum amount of input characters.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,13 @@
-
+from config import MAX_CHARS
 
 def get_file_content(working_directory: str, file_path: str) -> str:
-  if file_path not in working_directory:
-    raise Exception
+  if os.path.commonpath(file_path, working_directory):
+    raise Exception(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
+  if not os.path.isfile(file_path):
+    raise Exception(f'Error: File not found or is not a regular file: "{file_path}"')
+
+
+  with open(file_path, 'r') as f:
+    file_content_string = f.read(MAX_CHARS)
+  if f.read(1):
+    pass

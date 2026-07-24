04be0124b272bce6597e4a97f2b5744361dbddd7
tgybyrl <tgybyrl48@gmail.com>
2026-07-24 11:10:27 +0300

    System prompts created and tested. Also main.py structure fix.

--- main.py
+++ main.py
@@ -2,42 +2,44 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 import argparse
-
-load_dotenv()
-api_key = os.environ.get("OPENROUTER_API_KEY")
-if api_key is None:
-    raise RuntimeError("API key is not found! Enter API key.")
-
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key,
-)
-
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args  = parser.parse_args()
-
-messages = [
-    {"role": "user", "content": args.user_prompt},
-]
-
-response = client.chat.completions.create(model="openrouter/free", messages=messages,
-)
-
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage.prompt_tokens}")
-    print(f"Response tokens: {response.usage.completion_tokens}")
-
-print(response.choices[0].message.content)
+from prompts import system_prompt
 
 
-if response.usage is None:
-    raise RuntimeError
 
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None:
+        raise RuntimeError("API key is not found! Enter API key.")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args  = parser.parse_args()
+
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": args.user_prompt},
+    ]
+
+    response = client.chat.completions.create(model="openrouter/free", messages=messages,
+    )
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+
+    print(response.choices[0].message.content)
+
+
+    if response.usage is None:
+        raise RuntimeError
 
 
 if __name__ == "__main__":

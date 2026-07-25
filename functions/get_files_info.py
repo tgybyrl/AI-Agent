ab29c49dd2a903884d70099d72ed1e0ffb199f34
tgybyrl <tgybyrl48@gmail.com>
2026-07-25 22:34:34 +0300

    Typo fixed.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -38,7 +38,7 @@ schema_get_files_info = {
       "properties": {
         "directory":{
         "type": "string",
-        "desciption": "Directory path to list files from, relative to the working directory (default is the working directory itself)",
+        "description": "Directory path to list files from, relative to the working directory (default is the working directory itself)",
         }
       }
     }

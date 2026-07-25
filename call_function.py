1759920e95d577b5f7b5ecab2bf1c9c0557141b6
tgybyrl <tgybyrl48@gmail.com>
2026-07-25 22:36:59 +0300

    Added new schema.

--- call_function.py
+++ call_function.py
@@ -1,5 +1,7 @@
 from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
 
 available_functions = [
-  schema_get_files_info
+  schema_get_files_info,
+  schema_get_file_content,
 ]

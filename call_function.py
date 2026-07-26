54ec274b348ab182572da6fa2636b5cd5b3db297
tgybyrl <tgybyrl48@gmail.com>
2026-07-26 11:12:11 +0300

    Schema for run python file added to available functions.

--- call_function.py
+++ call_function.py
@@ -1,7 +1,10 @@
 from functions.get_files_info import schema_get_files_info
 from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
 
 available_functions = [
   schema_get_files_info,
   schema_get_file_content,
+  schema_run_python_file,
+  
 ]

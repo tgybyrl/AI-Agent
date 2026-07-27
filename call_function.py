d5ec00403de84c295a70c08234d319296dc4b4ac
tgybyrl <tgybyrl48@gmail.com>
2026-07-27 12:53:42 +0300

    Call function updated.

--- call_function.py
+++ call_function.py
@@ -1,7 +1,11 @@
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+import json
+
+from collections.abc import Callable
+
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
 
 available_functions = [
   schema_get_files_info,
@@ -9,3 +13,44 @@ available_functions = [
   schema_run_python_file,
   schema_write_file
 ]
+
+def call_function(tool_call, verbose: bool = False) -> dict:
+
+  #the name of the function
+  function_name = tool_call.function.name
+
+  function_args = json.loads(tool_call.function.arguments or "{}")
+  
+  if verbose == True:
+    print(f" - Calling function: {function_name}({function_args})")
+  else:
+    print(f" - Calling function: {function_name}")
+
+  function_map: dict[str, Callable[..., str]] = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "run_python_file": run_python_file,
+    "write_file": write_file,
+  }
+
+
+  if function_name not in function_map:
+    return {
+      "role": "tool",
+      "tool_call_id": tool_call.id,
+      "content": f"Error: Unknown function: {function_name}"
+    }
+  
+  else:
+    function_args["working_directory"] = "./calculator"
+
+    result = function_map[function_name](**function_args)
+
+    return {
+      "role": "tool",
+      "tool_call_id": tool_call.id,
+      "content": result,
+    }
+
+
+  
